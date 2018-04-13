41319ec21d104a421bb806d5221b94b41b555e79
hyunsik <hyunsik.dev@gmail.com>
2018-04-14 05:38:01 +0900

    Add redux module

---
+++ ProjectPurple/Web/FrontEnd/src/redux/modules/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import base from "./base";
+
+/**
+ * 리듀서를 이 곳에서 하나로 합쳐 내보내기
+ */
+
+export default combineReducers({
+  base
+});
--- ProjectPurple/Web/FrontEnd/src/shared/App.js
+++ ProjectPurple/Web/FrontEnd/src/shared/App.js
@@ -3,15 +3,18 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import { Home, Login, Signup, Find } from "../pages";
 
+/**
+ * 라우트 정의
+ */
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route exact path="/" component={Login} />
-
-        {/* <Route path="/login" component={Login} /> */}
         <Route path="/signup" component={Signup} />
         <Route path="/find" component={Find} />
+        <Route path="/home" component={Home} />
       </div>
     );
   }
