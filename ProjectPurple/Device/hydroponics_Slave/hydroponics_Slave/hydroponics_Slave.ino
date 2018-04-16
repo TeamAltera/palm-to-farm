4ef7d4bd034dabbecacc0182c8c9d1280c0b90fa
ahn <sencom1028@gmail.com>
2018-04-16 21:23:00 +0900

    smart plant control hardware code

---
+++ ProjectPurple/Device/hydroponics_Slave/hydroponics_Slave/hydroponics_Slave.ino
@@ -0,0 +1,116 @@
+﻿/*
+Name:		IR_slave.ino
+Created:	2018-02-12 오후 10:43:24
+Author:	AN
+*/
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
+#include <DHT11.h>
+char* userName = "";
+char* ssid = ""; //AP's ssid
+char* psw = ""; // AP's password
+
+char* server_ip = "203.250.32.37"; //웹 서버 아이피
+unsigned int server_port = 9001; //웹 서버 포트
+								 //웹 서버 아이피랑 포트가 변경되면 변경해줘야
+
+String uri = "/Aircontroll/setHumiAndTemp.Action"; //웹서버 uri경로
+												   //이부분은 웹개발자랑 서로 조율해야
+
+String bluetooth_cmd = "";
+boolean wifi_join = false;
+
+void esp8266Client_setup() {
+	Serial.begin(9600);
+	Serial1.begin(9600);
+	Serial2.begin(9600);
+	delay(1000);
+	sendData("AT+RST\r\n", 3000, 0); //esp리셋
+	sendData("AT\r\n", 2000, 0); //esp테스트
+	sendData("AT+CWMODE=1\r\n", 2000, 0); //esp모드는 스테이션
+}
+
+void esp8266_joinAP() {
+	sendData("AT+CWQAP\r\n", 2000, 0); //esp 연결된 AP접속 끊기
+	String join = String("AT+CWJAP=\"") + ssid + "\",\"" + psw + "\"\r\n";
+	sendData(join, 5000, 0); //esp 새로운 AP에 연결
+	if (sendData("AT+CWJAP?\r\n", 3000, 0).indexOf("OK") != -1) {
+		Serial.println("slave success");
+		Serial1.print('1');//송신 아두이노에게 성공함을 전송
+		sendData("AT+CIFSR\r\n", 2000, 0); //할당받은 아이피는?
+		wifi_join = true;
+	}
+	else {
+		Serial.println("slave fail");
+		Serial1.print('0');//송신 아두이노에게 실패함을 전송
+	}
+}
+
+//웹서버에 센싱한 값 전송, 매개변수랑 함수내부 코드 수정필요
+//복수개의 수경재배기 운용시, 어느 수경재배기인지 식별해줘야 되므로
+//쿼리스트링에 수경재배기 번호 포함해줘야
+void esp8266_send(String temp, String humi) {
+	String conn = String("AT+CIPSTART=\"TCP\"") + ",\"" + server_ip + "\"," + server_port + "\r\n";
+	if (sendData(conn, 3000, 0).indexOf("OK") == -1) {
+		return;
+	}
+
+	String query = "?userName=" + String(userName) + "&temp=" + temp + "&humi=" + humi;
+	String request = "GET " + uri + query + "\r\n";
+	request += "Connection:close\r\n\r\n";
+
+	sendData(String("AT+CIPSEND=") + request.length() + "\r\n", 1000, 0);
+	sendData(request, 1000, 0);
+	Serial2.flush();
+}
+
+//건드릴 필요 없음
+void bluetooth_set() {
+	StaticJsonBuffer<100> jsonBuffer;
+	JsonObject& jsonValue = jsonBuffer.parseObject(bluetooth_cmd);//json포맷으로 읽어온다
+																  //{userName:"~",ssid:"~",psw:"~"}
+	userName = jsonValue["userName"];//사용자 아이디
+	ssid = jsonValue["ssid"];//AP이름
+	psw = jsonValue["psw"];//AP패스워드
+}
+
+//건드릴 필요 없음
+String sendData(String command, long timeout, boolean debug) {
+	String response = "";
+	Serial2.print(command);
+	long time = millis();
+	while ((time + timeout) > millis()) {
+		while (Serial2.available()) {
+			char c = Serial2.read();
+			response += c;
+		}
+	}
+	if (!debug) {
+		Serial.println(response);
+	}
+	return response;
+}
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	esp8266Client_setup(); //esp설정
+	while (!Serial1.available()) {}//AVR스핀
+	delay(2000);
+	while (Serial1.available()) //UART에 값이 들어오면(마스터로부터 받은 값),
+	{
+		bluetooth_cmd += (char)Serial1.read(); //읽어들이고,
+	}
+	Serial.println(bluetooth_cmd);
+	bluetooth_set();//userName, AP ssid, AP PWD값 저장
+	esp8266_joinAP();//저장한 정보를 가지고 AP에 연결
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	if (wifi_join) {
+		//AP가 연결되어 있다면 센싱되는 정보 일정 주기마다 웹서버로 전송하는 코드
+		//포함해야
+	}
+	//이외에도 주기적으로 값을 센싱하다가 특정조건이 되었을때 자동으로 제어하는
+	//코드 포함해야
+}
---
+++ ProjectPurple/Device/hydroponics_master/hydroponics_master/hydroponics_master.ino
@@ -0,0 +1,214 @@
+﻿/*
+Name:		IR_master.ino
+Created:	2018-02-12 오후 7:35:00
+Author:	AN
+*/
+#include <ArduinoJson.h>
+char* userName = "";
+char* ssid = ""; //AP's ssid
+char* psw = ""; // AP's password
+
+boolean bluetooth_protocol = false; // '{' is true, '}' is false
+boolean wifi_join = false;
+String bluetooth_cmd = "";
+String buffer = "";
+int buffer_count = 0;
+
+const int success_led = 3;
+const int fail_led = 2;
+const int piezo = 4;
+
+void esp8266Server_setup() {
+	Serial.begin(9600);
+	Serial1.begin(9600);
+	Serial2.begin(9600);
+	delay(1000);
+	sendData("AT+RST\r\n", 3000, 0);
+	sendData("AT\r\n", 2000, 0);
+	sendData("AT+CWMODE=1\r\n", 2000, 0);
+	sendData("AT+CIPMUX=1\r\n", 2000, 0);
+	sendData("AT+CIPSERVER=1,80\r\n", 2000, 0);
+}
+
+String sendData(String command, long timeout, boolean debug) {
+	String response = "";
+	Serial2.print(command);
+	long time = millis();
+	while ((time + timeout) > millis()) {
+		while (Serial2.available()) {
+			char c = Serial2.read();
+			response += c;
+		}
+	}
+	if (!debug) {
+		Serial.println(response);
+	}
+	return response;
+}
+
+void esp8266_joinAP() {
+	change_led_state(0);//빨간불
+	sendData("AT+CWQAP\r\n", 2000, 0);
+	String join = String("AT+CWJAP=\"") + ssid + "\",\"" + psw + "\"\r\n";
+	sendData(join, 5000, 0);
+	if (sendData("AT+CWJAP?\r\n", 3000, 0).indexOf("OK") != -1) {
+		Serial1.print(bluetooth_cmd);
+		while (!Serial1.available()) {}
+		delay(1000);
+		while (Serial1.available())
+		{
+			char res = (char)Serial1.read();
+			Serial.println(res);
+			if (res != '1') {
+				return;
+			}
+		}
+		sendData("AT+CIFSR\r\n", 2000, 0);
+		change_led_state(1);//파란불
+		tone(piezo, 392);//AP연결 설정이 끝났으므로 소리로 알려준다.
+		delay(1000);
+		noTone(piezo);
+		wifi_join = true;
+		//AP나 서버로 현재 할당받은 내부 아이피 알려줘야 함
+		//AT+CIFSR로 얻은 아이피를 문자열에 저장해야
+		//라즈베리 AP측에서는 해당 정보를 가지고 포트포워딩 해야
+	}
+	else {
+		change_led_state(0);//빨간불
+	}
+}
+
+void change_led_state(boolean st) {
+	if (st == 0) {//빨강
+		digitalWrite(success_led, LOW);
+		digitalWrite(fail_led, HIGH);
+	}
+	else {//파랑
+		digitalWrite(success_led, HIGH);
+		digitalWrite(fail_led, LOW);
+	}
+}
+
+#if 1
+void esp8266_read() { //명령 라우팅
+	if (Serial2.available()) {
+		String temp = Serial2.readStringUntil('\n');
+		Serial.println("DEBUG: " + temp);
+		buffer += temp;
+		if (temp.charAt(0) == 13) {
+			buffer_count++;
+			if (buffer_count == 2) {//\r\n\r\n까지 받아오면 수행
+				buffer_count = 0;
+				unsigned int c_id = buffer.charAt(buffer.indexOf("+IPD,") + 5) - 48;
+				Serial.println(c_id);
+				int cmd = buffer.charAt(buffer.indexOf("cmd=") + 4) - 48;
+				Serial.println(cmd);
+				String content = "";
+				switch (cmd) //cmd 라우팅
+				{
+				case 1:
+
+					content = "on";
+					Serial.println(content);
+					break;
+				case 2:
+
+					content = "off";
+					Serial.println(content);
+					break;
+				default:
+					content = "err";
+					break;
+				}
+				buffer = ""; //String 버퍼 클리어
+
+				String response = "HTTP/1.1 200 OK\r\n";
+				response += "Content-Type:text/html;charset=UTF-8\r\n";//CORS 핸들링에  따른 content-type만 가능
+				response += "Content-Length:";
+				response += content.length();
+				response += "\r\n";
+				response += "Access-Control-Allow-Origin:*\r\n";//CORS
+				response += "Connection:close\r\n\r\n";//포함하지 않으면 esp 혼동
+													   //response += "Keep-Alive:timeout=5,max=999\r\n\r\n";
+				response += content;
+				sendData(String("AT+CIPSEND=") + c_id + "," + response.length() + "\r\n", 3000, 0);
+				sendData(response, 1000, 1);
+				Serial2.flush();
+			}
+		}
+		else buffer_count = 0;
+	}
+}
+#endif
+
+//복수개의 수경재배기 운용때 각 블루투스 이름 달리해줘야되므로
+//이름정해주는 부분 나중에 수정필요
+void bluetooth_setup() {
+	Serial3.begin(9600);
+	bluetooth_write("AT+NAMEplant", 12); //블루투스 이름
+	bluetooth_write("AT+PIN2014", 10); //블루투스 비번
+}
+
+//건드릴 필요 없음
+void bluetooth_read() {
+	while (!Serial3.available()) {}
+	delay(2000);
+	while (Serial3.available())
+	{
+		char temp = (char)Serial3.read();
+		if (temp == '{') {// 1. 첫번째 문자가 '{'라면 블루투스 통신시작
+			bluetooth_protocol = true;
+		}
+		else if (bluetooth_protocol && temp == '}') {// 3. 끝이 '}'라면 통신끝
+			bluetooth_protocol = false;
+			bluetooth_cmd += temp;
+
+			bluetooth_set(); //bluetooth_cmd에 저장된 정보를 읽어온다(userName,ssid,psw)
+			esp8266_joinAP();//esp랑 AP연결
+			bluetooth_cmd = "";//문자열을 비운다
+		}
+		if (bluetooth_protocol) {// 2. 블루투스 통신중이라면
+			bluetooth_cmd += temp;
+		}
+	}
+}
+
+//건드릴 필요 없음
+void bluetooth_set() {
+	StaticJsonBuffer<100> jsonBuffer;
+	JsonObject& jsonValue = jsonBuffer.parseObject(bluetooth_cmd);//json포맷으로 읽어온다
+																  //{userName:"~",ssid:"~",psw:"~"}
+	userName = jsonValue["userName"];//사용자 아이디
+	ssid = jsonValue["ssid"];//AP이름
+	psw = jsonValue["psw"];//AP패스워드
+}
+
+//건드릴 필요 없음
+void bluetooth_write(char* cmd, size_t len) {
+	for (size_t i = 0; i < len; i++)
+	{
+		Serial3.write(cmd[i]);
+	}
+	delay(2000);
+	while (Serial3.available()) Serial3.read();
+}
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	pinMode(success_led, OUTPUT);
+	pinMode(fail_led, OUTPUT);
+	digitalWrite(success_led, LOW);
+	digitalWrite(fail_led, LOW);//점멸등 설정
+
+	esp8266Server_setup(); //esp설정
+	bluetooth_setup(); //블루투스 설정, 블루투스 이름정해주는부분 나중에 수정필요
+	change_led_state(0);//빨간불
+	bluetooth_read(); //블루투스에 값이들어올때 까지 대기
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	if (wifi_join)
+		esp8266_read();
+}
