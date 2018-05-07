f7e3cc885339e4a97f805aa624aff3f5b050c9c0
hyunsik <hyunsik.dev@gmail.com>
2018-05-08 02:36:10 +0900

    Add Sample chart with chart.js

---
+++ ProjectPurple/Web/FrontEnd/src/pages/Chart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Chart extends Component {
+  
+  render() {
+    return(
+      <div>
+        chartjs
+      </div>
+    );
+  }
+}
