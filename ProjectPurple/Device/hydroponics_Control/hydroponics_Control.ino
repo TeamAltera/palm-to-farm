11417f8ec572d120f9d8f9567ec24344e70ddc37
LeeJeho <leejen2003@naver.com>
2018-05-24 20:30:43 +0900

    added code that works in manual mode.

--- ProjectPurple/Device/hydroponics_Control/hydroponics_Control.ino
+++ ProjectPurple/Device/hydroponics_Control/hydroponics_Control.ino
@@ -4,6 +4,7 @@
 #define RELAY_IN2 5
 #define RELAY_IN3 6
 #define RELAY_IN4 7
+#define RELAY_PUMP 8
 #define fan1 11
 #define fan2 12
 #define fan3 13		//냉각팬 핀번호
@@ -18,6 +19,12 @@ void Relay_Control(int cmd) {
 	else if (cmd == 5) {
 		for (int i = RELAY_IN1; i <= RELAY_IN4; i++)
 			digitalWrite(i, HIGH);
+	}
+	else if (cmd == 12) {
+		digitalWrite(RELAY_PUMP, LOW);
+	}
+	else if (cmd == 13) {
+		digitalWrite(RELAY_PUMP, HIGH);
 	}
 }
 
@@ -38,7 +45,7 @@ void fan_control(int cmd) {
 void setup()
 {
 	Serial.begin(9600);		//슬레이브 보드와 통신
-	Serial_M.begin(9600);	//마스터보 보드와 통신
+	Serial_M.begin(9600);	//마스터 보드와 통신
 	pinMode(fan1, OUTPUT);
 	pinMode(fan2, OUTPUT);
 	pinMode(fan3, OUTPUT);		//냉각팬 핀모드 : 출력
@@ -58,6 +65,30 @@ void loop()
 {
 	if (Serial.available()) {		//슬레이브 보드에서 전송한 데이터 존재할 경우
 		int cmd = Serial.parseInt();
+		switch (cmd) {
+		case 4:			//LED ON
+			Relay_Control(4);
+			break;
+		case 5:			//LED OFF
+			Relay_Control(5);
+			break;
+		case 8:			//FAN ON
+			fan_control(8);
+			break;
+		case 9:			//FAN OFF
+			fan_control(9);
+			break;
+		case 12:
+			Relay_Control(12);
+			break;
+		case 13:
+			Relay_Control(13);
+			break;
+		}
+	}
+
+	if (Serial_M.available()) {
+		int cmd = Serial_M.parseInt();
 		switch (cmd) {
 		case 4:			//LED ON
 			Relay_Control(4);
